872f2f22fb4eeb286961d52e54a2a5ea0bd7736a
Ankit kumar <103176206+ankitkumar55@users.noreply.github.com>
2023-04-29 14:18:41 +0530

    Create Just one more episode.cpp

---
+++ Coding Exercise Solutions/Just one more episode.cpp
@@ -0,0 +1,25 @@
+//https://www.codechef.com/problems/ONEMORE
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if(n > 24){
+            cout << "YES" << endl;
+            
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+	return 0;
+}
