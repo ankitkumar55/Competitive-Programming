724ba06e499247bf343293226362d874fab3a682
Ankit kumar <103176206+ankitkumar55@users.noreply.github.com>
2023-04-29 14:16:18 +0530

    Update and rename hello.cpp to Saving taxes.cpp

---
+++ Coding Exercise Solutions/Saving taxes.cpp
@@ -0,0 +1,20 @@
+//https://www.codechef.com/problems/TAXSAVING
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y; 
+        int c=x-y;
+        cout<<c<<endl;
+    }
+        return 0;
+}
